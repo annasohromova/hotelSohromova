4499a602df8eb68297795df595fa1acf8c83d47c
annasohromova <120181353+annasohromova@users.noreply.github.com>
2023-10-06 15:05:40 +0300

    Update indexid.sql
    lisanud kommentaarid

--- indexid.sql
+++ indexid.sql
@@ -1,13 +1,13 @@
 select * from DimEmployee
-create clustered index IX_tblEmployee_Name
+create clustered index IX_tblEmployee_Name --loome uus index
 on DimEmployee(FirstName)
 
-Drop index DimEmployee.PK_DimEmplo_3214EC070A9D95DB
+Drop index DimEmployee.PK_DimEmplo_3214EC070A9D95DB--kustutame index
 
-create clustered Index IX_tblEmployee_Gender_Salary
+create clustered Index IX_tblEmployee_Gender_Salary--loome uus clustered index
 on tblEmployee(Gender DESC, Salary ASC)
 
-create table [tblEmployee]
+create table [tblEmployee]--loome uus tabel
 (
 [ID] int Primary Key,
 [Name] varchar(50),
@@ -16,26 +16,26 @@ create table [tblEmployee]
 [City] varchar(50)
 )
 select * from tblEmployee
-insert into tblEmployee values(3, 'John', 4500, 'Male', 'New York')
+insert into tblEmployee values(3, 'John', 4500, 'Male', 'New York')--lisame väärtusi tabeli tblEmployee
 insert into tblEmployee values(1, 'Sam', 2500, 'Male', 'London')
 insert into tblEmployee values(4, 'Sara', 5500, 'Female', 'Tokyo')
 insert into tblEmployee values(5, 'Todd', 3100, 'Male', 'Toronto')
 insert into tblEmployee values(2, 'Pam', 6500, 'Female', 'Sydney')
 
-create NonClustered Index IX_tblEmployee_Salary
+create NonClustered Index IX_tblEmployee_Salary--loome none clustered indexi
 on tblEmployee(Salary ASC)
 
-Execute sp_helpindex tblEmployee;
+Execute sp_helpindex tblEmployee;--kontroll
 
-insert into tblEmployee values(1, 'Mike', 4500, 'Male', 'New York')
+insert into tblEmployee values(1, 'Mike', 4500, 'Male', 'New York')--lisame väärtusi
 insert into tblEmployee values(1, 'John', 2500, 'Male', 'London')
 
-drop index tblEmployee.PK_tblEmplo_3214EC07236943A5
+drop index tblEmployee.PK_tblEmplo_3214EC07236943A5--kustuta index
 
 select * from tblEmployee where Salary>4000 and Salary<8000
 
-delete from tblEmployee where Salary = 2500
-update tblEmployee set Salary=9000 where Salary=6500
+delete from tblEmployee where Salary = 2500--kustutame väärtusid mis on 2500 võrdub
+update tblEmployee set Salary=9000 where Salary=6500 --uuendame väärtusi
 
 select * from tblEmployee order by Salary;
 select * from tblEmployee order by Salary DESC;
